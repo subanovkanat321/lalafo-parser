10a200d524fc9c46fdd9bf897f35476a3e1addc6
Канат Эргешмамет уулу <subanovkanat321@gmail.com>
2026-01-20 22:53:41 +0300

    базовая структура Spring Boot и настройки Maven

---
+++ src/main/java/kg/lalafo/lalafo_service/LalafoServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.lalafo.lalafo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalafoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalafoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/lalafo/lalafo_service/LalafoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.lalafo.lalafo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalafoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
